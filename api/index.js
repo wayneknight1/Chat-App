10cb59076b6bd01a94a8e3f7668a137d14678a9b
wayneknight1 <pranavvenkata48@gmail.com>
2024-07-06 14:45:38 +0530

    Finished registration, login and the authentication part

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const dotenv = require('dotenv').config()
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 const User = require('./Models/User')
  
 app.use(express.json()); // Add this line to parse JSON bodies
@@ -18,6 +19,7 @@ mongoose.connect(process.env.MONGO_URI).then(() =>{
     console.log('mongoose connection successfull')
 })
 const jwt_secret = process.env.JWT_SECRET
+const bcryptSalt = bcrypt.genSaltSync(10)
 
 
 app.get('/profile',(req,res) => {
@@ -32,9 +34,25 @@ app.get('/profile',(req,res) => {
     }
 })
 
+app.post('/login',async (req,res)=>{
+    const {username, password} = req.body;
+    const foundUser = await User.findOne({username: username})
+    const passOk = bcrypt.compare(password, foundUser.password)
+    if(passOk){
+        jwt.sign({userId: foundUser._id, username: foundUser.username},jwt_secret,(err,token)=>{
+            if(err)
+                throw err;
+            res.cookie('token',token,{sameSite:'none',secure: true}).json({
+                id: foundUser._id
+            })
+        })
+    }
+})
+
 app.post('/register',async (req,res) => {
     const {username, password} = req.body
-    const createdUser = await User.create({username, password})
+    const hashedPassword = bcrypt.hashSync(password,bcryptSalt)
+    const createdUser = await User.create({username, password: hashedPassword})
     jwt.sign({userId: createdUser._id, username},jwt_secret,(err,token) =>{
         if(err)
             throw err;
